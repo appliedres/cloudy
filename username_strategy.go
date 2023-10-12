d1d5f855f51c34ee3e4855739a5338ab146d37bc
Adam Dyer <adamdyer@gmail.com>
2023-10-12 17:20:59 -0400

    Updated and added more logging around username guessing

--- username_strategy.go
+++ username_strategy.go
@@ -10,9 +10,11 @@ import (
 
 type DuplicateUserMode int
 
-const UserAny DuplicateUserMode = 1
-const UserExists DuplicateUserMode = 2
-const UserDoesNotExist DuplicateUserMode = 3
+const UserAny DuplicateUserMode = 0
+const UserExists DuplicateUserMode = 1
+const UserDoesNotExist DuplicateUserMode = 2
+
+var DuplicateUserNames = [3]string{"UserAny", "UserExists", "UserDoesNotExist"}
 
 type UsernameStrategy interface {
 	// GuessUsername provides the best username for a given user object. The pattern should contain
@@ -22,50 +24,51 @@ type UsernameStrategy interface {
 type FirstDotLastUsernameStrategy struct{}
 
 func (strat *FirstDotLastUsernameStrategy) GuessUsername(ctx context.Context, user *models.User, mode DuplicateUserMode, manager UserManager, userDomain string) (string, error) {
-	username := user.FirstName + "." + user.LastName
+	username := strings.ToLower(user.FirstName + "." + user.LastName)
 	return FindMatchingUser(ctx, username, mode, manager, userDomain)
 }
 
 type FirstInitialLastUsernameStrategy struct{}
 
 func (strat *FirstInitialLastUsernameStrategy) GuessUsername(ctx context.Context, user *models.User, mode DuplicateUserMode, manager UserManager, userDomain string) (string, error) {
-	username := user.FirstName[:1] + user.LastName
+	username := strings.ToLower(user.FirstName[:1] + user.LastName)
 	return FindMatchingUser(ctx, username, mode, manager, userDomain)
 }
 
 func FindMatchingUser(ctx context.Context, username string, mode DuplicateUserMode, manager UserManager, userDomain string) (string, error) {
 
-	guess := addDomain(username, userDomain)
+	lowerUser := strings.ToLower(username)
+	lowerDomain := strings.ToLower(userDomain)
+	guess := AddDomain(lowerUser, lowerDomain)
+
 	if mode == UserAny {
 		return guess, nil
 	}
 
-	for count := 1; count < 100; count++ {
+	for count := 1; count < 20; count++ {
 		if count > 1 {
-			guess = addDomain(fmt.Sprintf("%v.%v", username, count), userDomain)
+			guess = AddDomain(fmt.Sprintf("%v.%v", lowerUser, count), lowerDomain)
 		}
 
 		user, err := manager.GetUser(ctx, guess)
 		if err != nil {
-			return guess, nil
+			return "", Error(ctx, "Error getting user during FindMatchingUser: %v", err)
 		}
 
 		if mode == UserExists && user != nil {
+			Info(ctx, "UserExists -> guessing username %s", guess)
 			return guess, nil
-		}
-		if mode == UserExists && user == nil {
-			return "", nil
 		}
 		if mode == UserDoesNotExist && user == nil {
+			Info(ctx, "UserDoesNotExist -> guessing username %s", guess)
 			return guess, nil
 		}
-
-		Info(ctx, "Found User %v", user.DisplayName)
 	}
-	return "", nil
+
+	return "", Error(ctx, "Unable to find a matching user for username: %s (mode: %s)", lowerUser, DuplicateUserNames[mode])
 }
 
-func addDomain(user string, domain string) string {
+func AddDomain(user string, domain string) string {
 	if domain != "" {
 		return user + "@" + domain
 	}
